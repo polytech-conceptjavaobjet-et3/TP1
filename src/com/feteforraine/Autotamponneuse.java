16ef8a3218825bba1c408257747d5e26bda8587f
Eugénie Brasier <eugenie.brasier@gmail.com>
2020-03-11 14:14:19 +0100

    Test commit

---
+++ src/com/feteforraine/Autotamponneuse.java
@@ -0,0 +1,19 @@
+package com.feteforraine;
+
+public class Autotamponneuse 
+{
+	//variables
+	
+	// => le fait qu’elles soient placées ou non sur une piste d’autotamponneuses, leur position étant alors représentée par une paire de valeurs réelles (coordonnées dans un plan à deux dimensions)
+	private int[] position = new int[2];
+	
+	// => le fait qu’elles soient occupées ou non, et si oui par quelle personne (il faut qu’elles soient placées pour pouvoir être occupées)
+	private 
+	
+	// => le fait qu’elle soient allumées ou non (il faut qu’elles soient occupées pour pouvoir être allumées)
+	private boolean allumee = false;
+	
+	// => le fait qu’elle soient clignotantes ou non (il faut qu’elles soient allumées pour pouvoir être cignotantes)
+	private boolean clignotante = false;
+
+}
